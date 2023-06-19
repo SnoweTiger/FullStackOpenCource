aed489e5a8abe290e46f871334ea2a0bbf19acd3
SnoweTiger <snowetiger@gmail.com>
2023-06-19 19:43:30 +0300

    1.1: course information, step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,22 +1,33 @@
+const Header = ({title}) => (
+  <h1>{title}</h1>
+)
 
+const Content = ({part, exercises}) => (
+  <p>{part} {exercises}</p>
+)
 
-
-const Hello = (props) => {
-  return (
-    <div>
-      Hello {props.name}, you are {props.age} years old
-    </div>
-  )
-}
+const Total = ({total}) => (
+  <p>Number of exercises {total}</p>
+)
 
 const App = () => {
-  const friends = [ 'Peter', 'Maya']
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <p>{friends}</p>
+      <Header title={course}/>
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
 
-export default App;
+export default App
