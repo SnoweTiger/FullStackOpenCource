9a565e4de365ea38243400d9c4553fdbd375b7b8
SnoweTiger <snowetiger@gmail.com>
2023-06-19 20:13:50 +0300

    1.5: course information step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -19,28 +19,31 @@ const Total = ({total}) => (
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
 
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
 
   return (
     <div>
-      <Header title={course}/>
-      <Content data={parts}/>
+      <Header title={course.name}/>
+      <Content data={course.parts}/>
       <Total total={total}/>
     </div>
   )
