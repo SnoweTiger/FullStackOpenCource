174f6ab78eb8dc868cee93962f606f64ffb1216c
SnoweTiger <snowetiger@gmail.com>
2023-06-20 15:00:18 +0300

    2.2: Course information step7

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,13 +1,20 @@
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
+
 
 const Course = ({course}) => {
+
+    const totalExercises = course.parts.reduce((subTotal, part) => subTotal + part.exercises, 0)
+
     return (
       <div>
         <Header title={course.name} />
         <Content data={course.parts} />
+        <Total total={totalExercises} />
       </div>   
     )
+
 }
 
 export default Course
