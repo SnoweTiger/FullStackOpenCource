0d517b5f98ba603840ccfa91050eb1c14ef1314a
SnoweTiger <snowetiger@gmail.com>
2023-06-20 15:07:40 +0300

    2.4-5: Course information step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,48 +2,61 @@ import Course from "./components/Course"
 
 
 
-// const Total = ({total}) => (
-//   <p>Number of exercises {total}</p>
-// )
-
-
-
 const App = () => {
 
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-    ]
-  }
-
-  // Test course change 
-  course.parts.push(
+  const courses = [
     {
-      name: 'Part X',
-      exercises: 999,
-      id: 999
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      {courses.map((course) => 
+        <Course course={course} key={course.id}/>
+      )}
+    </div>
   )
 
-  // const total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-
-  return <Course course={course} />
 }
 
 export default App
