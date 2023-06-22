a45cfe8e0accd906387b5b15990f2ccda2749206
SnoweTiger <snowetiger@gmail.com>
2023-06-22 17:25:24 +0300

    fix server port

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -98,7 +98,7 @@ app.post('/api/persons', (request, response) => {
   response.status(200).json(newPerson).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
