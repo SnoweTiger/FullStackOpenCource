ee13c1158088f6316875db93ac742a90fcde5b79
SnoweTiger <snowetiger@gmail.com>
2023-06-28 18:25:51 +0300

    4.17: bloglist expansion, step5

--- part4/blog/controllers/user.js
+++ part4/blog/controllers/user.js
@@ -3,7 +3,9 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 userRouter.get('/', async (request, response) => {
-    const users = await User.find({})  
+    const users = await User
+        .find({})
+        .populate('blogs', { title: 1, author: 1 })  
     response.json(users)  
 })
 
@@ -19,7 +21,11 @@ userRouter.post('/', async (request, response) => {
     const saltRounds = 9
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
-    const user = new User({username, name, passwordHash})
+    const user = new User({
+        username, 
+        name, 
+        passwordHash
+    })
 
     const savedUser = await user.save()
 
