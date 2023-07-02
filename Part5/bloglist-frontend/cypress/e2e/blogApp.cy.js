09c0fbe7807789429b7d5fccd70cebccbec5ddee
SnoweTiger <snowetiger@gmail.com>
2023-07-02 14:36:34 +0300

    5.18: bloglist end to end testing, step2

--- Part5/bloglist-frontend/cypress/e2e/blogApp.cy.js
+++ Part5/bloglist-frontend/cypress/e2e/blogApp.cy.js
@@ -1,6 +1,14 @@
 describe('Blog app', function() {
+
+    const testUser = {
+        name: 'Test User',
+        username: 'testLogin',
+        password: 'testPass'
+    }
+
     beforeEach(function() {
         cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+        cy.request('POST', `${Cypress.env('BACKEND')}/users/`, testUser)
         cy.visit('http://localhost:3000')
     })
 
@@ -8,5 +16,21 @@ describe('Blog app', function() {
         cy.get('input[name="Username"]').should('exist').should('be.empty')
         cy.get('input[name="Password"]').should('exist').should('be.empty')
         cy.contains('login').should('exist')
+    })
+
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('input[name="Username"]').type(testUser.username)
+            cy.get('input[name="Password"]').type(testUser.password)
+            cy.contains('login').click()
+            cy.contains(testUser.name)
+        })
+
+        it('fails with wrong credentials with message', function() {
+            cy.get('input[name="Username"]').type(`${testUser.username}1`)
+            cy.get('input[name="Password"]').type(testUser.password)
+            cy.contains('login').click()
+            cy.contains('invalid username or password').should('have.css', 'color', 'rgb(255, 0, 0)')
+        })
     })
 })
