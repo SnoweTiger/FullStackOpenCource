e79685eb1ca53c6990c8b39a7ceaaba1cbab4c63
SnoweTiger <snowetiger@gmail.com>
2023-07-06 19:25:37 +0300

    Exercise 6.20

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,40 +1,87 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
 const App = () => {
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+    const { isLoading, isError, data, error } = useQuery(
+        'anecdotes',
+        () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+        {
+            retry: 1
+        },
+        
+    )
+    console.log(isLoading, isError, data, error)
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+    if ( isLoading ) {
+        return <div>loading data...</div>
+    }
+    
+    if ( isError ) {
+        return <span>Error: {error.message}</span>
+    }
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
+    const anecdotes = data
+
+    const handleVote = (anecdote) => {
+        console.log('vote')
+    }
+
+    return (
+        <div>
+        <h3>Anecdote app</h3>
+        
+        <Notification />
+        <AnecdoteForm />
+        
+        {anecdotes.map(anecdote =>
+            <div key={anecdote.id}>
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
+            </div>
+        )}
         </div>
-      )}
-    </div>
-  )
+    )
 }
+
+  
+
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   },
+  // ]
+
+  // return (
+  //   <div>
+  //     <h3>Anecdote app</h3>
+    
+  //     <Notification />
+  //     <AnecdoteForm />
+    
+  //     {anecdotes.map(anecdote =>
+  //       <div key={anecdote.id}>
+  //         <div>
+  //           {anecdote.content}
+  //         </div>
+  //         <div>
+  //           has {anecdote.votes}
+  //           <button onClick={() => handleVote(anecdote)}>vote</button>
+  //         </div>
+  //       </div>
+  //     )}
+  //   </div>
+  // )
+
 
 export default App
