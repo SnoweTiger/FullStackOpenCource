be0a7619c676f30a2dbf799cfe899acafd512961
SnoweTiger <snowetiger@gmail.com>
2023-07-18 21:43:16 +0300

    8.10: Adding a book

---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`;
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      id
+      published
+      author
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation AddBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      author
+      id
+      published
+      title
+      genres
+    }
+  }
+`;
