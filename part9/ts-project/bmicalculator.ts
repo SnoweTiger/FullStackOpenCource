aea512ea4d9b75dc94c4a27b1ec51e7eed704687
SnoweTiger <snowetiger@gmail.com>
2023-07-27 22:18:23 +0300

    9.5 WebBMI

--- part9/ts-project/bmicalculator.ts
+++ part9/ts-project/bmicalculator.ts
@@ -1,15 +1,19 @@
 
-const calculateBmi = (height: number, weight: number) : string => {
-    const bmi = weight / (height * height)
+const calculateBmi = (height: number, weight: number): string => {
+    const heightM : number = height / 100
+    const bmi = weight / (heightM * heightM)
     if (bmi <= 25) {
         return 'Normal'
     } else if (bmi >= 30) {
-        return 'Overweight'
-    } else {
         return 'Obese'
+    } else {
+        return 'Overweight'
     }
  }
 
 const h: number = Number(process.argv[3])
 const w: number = Number(process.argv[4])
+
 console.log(calculateBmi(h, w))
+
+export default calculateBmi
--- part9/ts-project/index.ts
+++ part9/ts-project/index.ts
@@ -1,9 +1,34 @@
 import express from 'express';
+import calculateBmi from "./bmicalculator";
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+
+    const height: number = Number(req.query.height)
+    const weight: number = Number(req.query.weight)
+    
+    if (height && weight) {
+        res.send(
+            {
+                height: height,
+                weight: weight,
+                bmi: calculateBmi(height, weight)
+            }
+        );
+    } else {
+        res.send(
+            {
+                error: "malformatted parameters"
+            })
+    }    
+});
+
+
 
 const PORT = 3003;
 
